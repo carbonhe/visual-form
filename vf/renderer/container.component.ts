7e68e8feba65bbb4ac57abdb902d58b3b6b3da42
CodingCloud9527 <CodingCloud9527@gmail.com>
2020-12-20 14:42:56 +0800

    feat: add event in VfContainer component when render completed

--- vf/renderer/container.component.ts
+++ vf/renderer/container.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ControlSetting } from '../workspace/types';
 import { VfFormControl, VfFormGroup } from './types';
 import { PluginService } from '../plugable/plugin.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'vf-container',
@@ -12,7 +13,11 @@ import { PluginService } from '../plugable/plugin.service';
 })
 export class VfContainerComponent implements OnInit {
   @Input() controls: ControlSetting[] = [];
+
+  @Output() renderCompleted = new EventEmitter<FormGroup>();
+
   vf: VfFormGroup;
+
 
   constructor(private pluginService: PluginService) {
 
@@ -25,6 +30,7 @@ export class VfContainerComponent implements OnInit {
     });
 
     this.vf = new VfFormGroup<any>(this.pluginService.platform.rootGroup, vfControls);
+    this.renderCompleted.emit(this.vf);
   }
 
 
