5fae214954fc6ac9aad80f18dccedb15be50ade9
CodingCloud9527 <CodingCloud9527@gmail.com>
2020-12-26 17:12:39 +0800

    fix: fix exports

--- lib/antd/index.ts
+++ lib/antd/index.ts
@@ -6,8 +6,12 @@ export * from './contorl-components/option-setting.component';
 export * from './contorl-components/script-setting.component';
 export * from './contorl-components/select.component';
 export * from './contorl-components/textarea.component';
+export * from './contorl-components/checkbox.component';
+export * from './contorl-components/date.component';
+export * from './contorl-components/radio.component';
 
 export * from './controls/option-setting.control';
+export * from './controls/checkbox.control';
 
 export * from './groups/div-group.component';
 export * from './groups/form-group.component';
